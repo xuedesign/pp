e2c89ef19b7bbb41ad79d3d0c1f95862f7289e18
xuedesign <elmo@xuedesign.tw>
2013-04-22 18:33:14 +0800

    票卡編輯頁 進度85% #20130422

--- script/pass_edit.js
+++ script/pass_edit.js
@@ -2,10 +2,55 @@
 $(function(){
          $("#input-logotext").keyup(function(){
          $("#logo-text").empty();
-         $("#logo-text").text($(this).attr("value"));
+         $("#logo-text").text($(this).attr("value"));	 
       });
-    });	
+    });
+$(function(){
+         $("#input-prilabel").keyup(function(){
+         $("#primary_label").empty();
+         $("#primary_label").text($(this).attr("value"));	 
+      });
+    });				
+$(function(){
+         $("#input-privalue").keyup(function(){
+         $("#primary_value").empty();
+         $("#primary_value").text($(this).attr("value"));	 
+      });
+    });
+$(function(){
+         $("#input-seclabel-01").keyup(function(){
+         $("#sec01-label").empty();
+         $("#sec01-label").text($(this).attr("value"));	 
+      });
+    });
+$(function(){
+         $("#input-secvalue-01").keyup(function(){
+         $("#sec01-value").empty();
+         $("#sec01-value").text($(this).attr("value"));	 
+      });
+    });
+$(function(){
+         $("#input-seclabel-02").keyup(function(){
+         $("#sec02-label").empty();
+         $("#sec02-label").text($(this).attr("value"));	 
+      });
+    });
+$(function(){
+         $("#input-secvalue-02").keyup(function(){
+         $("#sec02-value").empty();
+         $("#sec02-value").text($(this).attr("value"));	 
+      });
+    });
+$(function(){
+         $("#input-altext").keyup(function(){
+         $("#barcode-text").empty();
+         $("#barcode-text").text($(this).attr("value"));	 
+      });
+    });					
 	
+		
 //顏色改變
 
-//QR code製作	
+
+//QR code製作
+
